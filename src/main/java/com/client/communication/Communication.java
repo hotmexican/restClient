92b076797d47d46a74e0b6c6b37d9eea4099fb2c
hotmexican <borro2885@gmail.com>
2022-05-20 01:03:47 +0300

    added views update controller

--- src/main/java/com/client/communication/Communication.java
+++ src/main/java/com/client/communication/Communication.java
@@ -7,7 +7,9 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
@@ -16,7 +18,7 @@ import java.util.List;
 public class Communication {
 
     private final RestTemplate restTemplate;
-    private final String URL = "http://localhost:8080/timeTracker/api/students";
+    private final String URL = "http://localhost:8081/timeTracker/api/students";
 
     @Autowired
     public Communication(RestTemplate restTemplate) {
@@ -34,14 +36,33 @@ public class Communication {
     }
 
     public Student getStudent(int id) {
+        Student student = restTemplate.getForObject(URL + "/" + id, Student.class);
         return null;
     }
 
-    public void saveStudent(Student student) {
+    @RequestMapping("/addNewStudent")
+    public String addNewStudent(Model model) {
+        Student student = new Student();
+        model.addAttribute("student", student);
+        return "add_student_view";
+    }
+    @RequestMapping("/saveStudent")
+    public String saveStudent(@ModelAttribute("student") Student student){
+        restTemplate.postForEntity(URL, student, String.class);
+        return "redirect:/";
+    }
+    @RequestMapping("/updateInfo")
+    public String updateStudent(@RequestParam("studentId") int id, Model model){
+        Student student = restTemplate.getForObject(URL + "/" + id, Student.class);
+        model.addAttribute("student", student);
+        restTemplate.put(URL, student);
+        return "add_student_view";
     }
 
-    public void deleteStudent(int id) {
-
+    @RequestMapping("/deleteStudent")
+    public String deleteStudent(@RequestParam("studentId") int id) {
+        restTemplate.delete(URL + "/" + id);
+        return "redirect:/";
     }
 
 }
--- src/main/webapp/WEB-INF/view/first_view.jsp
+++ src/main/webapp/WEB-INF/view/first_view.jsp
@@ -8,12 +8,26 @@
     <tr>
         <th>Name</th>
         <th>Surname</th>
+        <th>Operations</th>
     </tr>
     <c:forEach var="stud" items="${allStudents}">
+        <c:url var="updateButton" value="/updateInfo">
+            <c:param name="studentId" value="${stud.id}"/>
+        </c:url>
+        <c:url var="deleteButton" value="/deleteStudent">
+            <c:param name="studentId" value="${stud.id}"/>
+        </c:url>
         <tr>
             <td>${stud.name}</td>
             <td>${stud.surname}</td>
+            <td>
+                <input type="button" value="UPDATE" onclick="window.location.href='${updateButton}'">
+                <input type="button" value="DELETE" onclick="window.location.href='${deleteButton}'">
+            </td>
+
         </tr>
     </c:forEach>
 </table>
+<br>
+<input type="button" value="ADD" onclick="window.location.href='addNewStudent'"/>
 </body>
